e7cd205db4629d33e5f14b5c34e96c5d184eee0b
visak06 <78645279+visak06@users.noreply.github.com>
2021-02-06 18:50:32 +0530

    modied files

--- main/script.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll(".nav-link");
-links.forEach((item)=>{
-    item.addEventListener("click",()=>{
-        const el = document.getElementById(item.getAttribute("data-link"));
-        el.scrollIntoView({behavior:"smooth",block:"start"})
-    })
-})
